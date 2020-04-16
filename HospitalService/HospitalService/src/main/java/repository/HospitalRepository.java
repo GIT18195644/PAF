a8bcdec2f60ff4ff65fe2b2adbcc8d9915eba683
GIT18195644 <it18195644@my.sliit.lk>
2020-04-16 18:10:54 +0530

    DB Connection Created

--- HospitalService/HospitalService/src/main/java/repository/HospitalRepository.java
+++ HospitalService/HospitalService/src/main/java/repository/HospitalRepository.java
@@ -1,5 +1,22 @@
 package repository;
 
-public class HospitalRepository {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class HospitalRepository {
+Connection con = null;
+	
+	public HospitalRepository() {
+		String dbURL = "jdbc:mysql://127.0.0.1:3306/ayurwedha" ;
+		String dbUsername = "root" ;
+		String dbPassword = "";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = (Connection) DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 }
