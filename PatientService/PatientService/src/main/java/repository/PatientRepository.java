d7a06c3edfafc7f7931e59060452e659a57df2c0
ThiliniDureksha <dureksha97@gmail.com>
2020-04-16 14:06:25 +0530

    create DB connector

--- PatientService/PatientService/src/main/java/repository/PatientRepository.java
+++ PatientService/PatientService/src/main/java/repository/PatientRepository.java
@@ -2,6 +2,7 @@ package repository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -27,36 +28,131 @@ public class PatientRepository {
 	}
 	
 
-	public List<Patient> getPatients() {
+	//public List<Patient> getPatients() {
 		
-		List<Patient> patients = new ArrayList<>();
+		//List<Patient> patients = new ArrayList<>();
 		
-		String sql = "SELECT * FROM patientmanagement";
+		//String sql = "SELECT * FROM patientmanagement";
 		
-		try {
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
+		//try {
+			//Statement st = con.createStatement();
+			//ResultSet rs = st.executeQuery(sql);
 			
-			while (rs.next()) {
-				Patient p = new Patient();
+			//while (rs.next()) {
+				//Patient p = new Patient();
 				
-				p.setPatient_id(rs.getInt(1));
-				p.setName(rs.getString(2));
-				p.setGender(rs.getString(3));
-				p.setBirthday(rs.getDate(4));
-				p.setNic(rs.getString(5));
-				p.setPhone(rs.getString(6));
-				p.setUsername(rs.getString(7));
-				p.setPassword(rs.getString(8));
+				//p.setPatient_id(rs.getInt(1));
+				//p.setName(rs.getString(2));
+				//p.setGender(rs.getString(3));
+				//p.setBirthday(rs.getDate(4));
+				//p.setNic(rs.getString(5));
+				//p.setPhone(rs.getString(6));
+				//p.setUsername(rs.getString(7));
+				//p.setPassword(rs.getString(8));
 				
-				patients.add(p);
+				//patients.add(p);
 				
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+			//}
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}
 		
-		return patients;
-	}
+		//return patients;
+	//}
+
+
+	//public Patient getPatient(int patient_id) {
+		
+		//String sql = "SELECT * FROM patientmanagement WHERE Patient_id="+patient_id;
+		//Patient p = new Patient();
+		
+		//try {
+			//Statement st = con.createStatement();
+			//ResultSet rs = st.executeQuery(sql);
+			
+			//if (rs.next()) {
+				//p.setPatient_id(rs.getInt(1));
+				//p.setName(rs.getString(2));
+				//p.setGender(rs.getString(3));
+				//p.setBirthday(rs.getDate(4));
+				//p.setNic(rs.getString(5));
+				//p.setPhone(rs.getString(6));
+				//p.setUsername(rs.getString(7));
+				//p.setPassword(rs.getString(8));
+			//}
+			
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}
+		
+		//return p;
+	//}
+
+
+	//public void create(Patient p1) {
+		
+		//String sql = "INSERT INTO patientmanagement VALUES (?,?,?,?,?,?,?,?) ";
+		
+		//try {
+			//PreparedStatement st = con.prepareStatement(sql);
+			
+			//st.setInt(1, p1.getPatient_id());
+			//st.setString(2, p1.getName());
+			//st.setString(3,  p1.getGender());
+			//st.setDate(4, p1.getBirthday());
+			//st.setString(5, p1.getNic());
+			//st.setString(6,  p1.getPhone());
+			//st.setString(7, p1.getUsername());
+			//st.setString(8,  p1.getPassword());
+			
+			//st.executeUpdate();
+			
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}
+		
+	//}
+
+
+	//public void update(Patient p1) {
+		
+		//String sql = "UPDATE patientmanagement SET Name=?,Gender=?,Birthday=?,Nic=?,Phone=?,username=?,password=? WHERE Patient_id=?";
+		
+		//try {
+			//PreparedStatement st = con.prepareStatement(sql);
+			
+			//st.setInt(8, p1.getPatient_id());
+			//st.setString(1, p1.getName());
+			//st.setString(2, p1.getGender());
+			//st.setDate(3, p1.getBirthday());
+			//st.setString(4, p1.getNic());
+			//st.setString(5, p1.getPhone());
+			//st.setString(6,  p1.getUsername());
+			//st.setString(7,  p1.getPassword());
+			
+			//st.executeUpdate();
+		
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}
+		
+	//}
+
+
+	//public void delete(int patient_id) {
+		//String sql = "DELETE FROM patientmanagement WHERE Patient_id=?";
+		
+		//try {
+			//PreparedStatement st = con.prepareStatement(sql);
+			
+			//st.setInt(1, patient_id);
+			
+			//st.executeUpdate();
+			
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}
+		
+	//}
 
 }
--- PatientService/PatientService/src/main/java/resource/PatientResource.java
+++ PatientService/PatientService/src/main/java/resource/PatientResource.java
@@ -2,26 +2,111 @@ package resource;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import model.Patient;
 import repository.PatientRepository;
 
-@Path("patients")
-public class PatientResource {
+//@Path("patients")
+//public class PatientResource {
 	
-	PatientRepository prepo = new PatientRepository();
+	//PatientRepository prepo = new PatientRepository();
 	
 	//View Patient details
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Patient> getPatients(){
-		System.out.println("get patients...");
+	//@GET
+	//@Produces(MediaType.APPLICATION_JSON)
+	//public List<Patient> getPatients(){
+		//System.out.println("get patients...");
 		
-		return prepo.getPatients();
-	}
+		//return prepo.getPatients();
+	//}
+	
+//	//View own account details
+//	@GET
+//	@Path("bio/{current_patient_id}")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public List<Patient> getPatientDetails() {
+//		System.out.println("get patients...");
+//		Patient p = prepo.getPatient(current_patient_id);
+//		
+//		
+//		return prepo.getPatients();
+//	}
+	
+	//Search patient using patient id
+	//@GET
+	//@Path("search/{Patient_id}")
+	//@Produces(MediaType.APPLICATION_JSON)
+	//public Patient getPatient(@PathParam("Patient_id") int Patient_id) {
+		
+		//return prepo.getPatient(Patient_id);
+		
+	//}
+	
+	//Create patient
+	//@POST
+	//@Path("create")
+	//public Patient createPatient(Patient p1) {
+		//System.out.println(p1);
+		//prepo.create(p1);
+			
+		//return p1;
+	//}
+	
+		
+	//Update Patient
+	//@PUT
+	//@Path("update")
+	//public Patient updatePatient(Patient p1) {
+		//System.out.println(p1);
+			
+	//if (prepo.getPatient(p1.getPatient_id()).getPatient_id()==0) {
+	//System.out.println("DB have no details in this patient id");
+	//}
+	//else {
+		//prepo.update(p1);
+		//}
+			
+		//return p1;
+	//}
+	
+	//Deactivate account
+	//@DELETE
+	//@Path("deactivate/{Patient_id}")
+	//public Patient deactivateAccount(@PathParam("Patient_id") int Patient_id) {
+		//Patient p = prepo.getPatient(Patient_id);
+		
+		//if (p.getPatient_id() !=0) {
+			//prepo.delete(Patient_id);
+		//}
+		
+		//return p;
+		
+	//}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
