7db29b85b01a4259fc17ec938dfa76a42d1b4017
ThiliniDureksha <dureksha97@gmail.com>
2020-04-18 16:56:45 +0530

    appointment creation

--- PatientService/PatientService/src/main/java/repository/PatientRepository.java
+++ PatientService/PatientService/src/main/java/repository/PatientRepository.java
@@ -183,4 +183,70 @@ public class PatientRepository {
 		
 	}
 
+
+	//public Appointment getAppointmentAdmin(int appointment_id) {
+		
+		//String sql = "SELECT * FROM appointmentmanagement where appointment_id=" + appointment_id;
+		//Appointment a = new Appointment();
+
+		//try {
+			//Statement st = con.createStatement();
+			//ResultSet rs = st.executeQuery(sql);
+
+			//if (rs.next()) {
+				//a.setAppointment_id(rs.getInt(1));
+				//a.setAppointment_type(rs.getInt(2));
+				//a.setAppointment_date(rs.getString(3));
+				//a.setAppointment_time(rs.getString(4));
+				//a.setAppointment_fees(rs.getString(5));
+				//a.setPatient_id(rs.getInt(6));
+				//a.setHospital_id(rs.getInt(7));
+				//a.setDoctor_id(rs.getInt(8));
+			//}
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}
+		
+		//return a;
+	//}
+
+
+	//public void deleteAppointment(int patient_id, int appointment_id) {
+		//String sql = "DELETE FROM appointmentmanagement WHERE Patient_id=? AND appointment_id=?";
+
+		//try {
+			//PreparedStatement st = con.prepareStatement(sql);
+
+			//st.setInt(1, appointment_id);
+			//st.setInt(2, patient_id);
+
+			//st.executeUpdate();
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}		
+	//}
+
+
+	//public void updateAppointment(Appointment a1) {
+		//String sql = "UPDATE appointmentmanagement SET Appointment_type=?, Appointment_date=?, Appointment_time=?, Appointment_fees=?, Patient_id=?, Hospital_id=?, Doctor_id=? WHERE appointment_id=?";
+
+		//try {
+			//PreparedStatement st = con.prepareStatement(sql);
+
+			//st.setInt(1, a1.getAppointment_type());
+			//st.setString(2, a1.getAppointment_date());
+			//st.setString(3, a1.getAppointment_time());
+			//st.setString(4, a1.getAppointment_fees());
+			//st.setInt(5, a1.getPatient_id());
+			//st.setInt(6, a1.getHospital_id());
+			//st.setInt(7, a1.getDoctor_id());
+
+			//st.setInt(8, a1.getAppointment_id());
+
+			//st.executeUpdate();
+		//} catch (Exception e) {
+			//System.out.println(e);
+		//}
+	//}
+
 }
